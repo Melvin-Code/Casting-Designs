31374b189038f9bec08861e2f0dd38408d554357
melvin-Code <melvingonzalez470@gmail.com>
2020-06-11 17:13:51 -0400

    working on fetures

--- src/components/partials/Form.jsx
+++ src/components/partials/Form.jsx
@@ -7,9 +7,9 @@ export default class Form extends React.Component {
     this.state = {
       status: "",
       next: 0,
-      in1: '',
-      in2: '',
-      in3: '',
+      in1: "",
+      in2: "",
+      in3: "",
     };
   }
   handleChange = (e) => {
@@ -17,25 +17,56 @@ export default class Form extends React.Component {
       [e.target.name]: e.target.value,
     });
   };
-  nextCounter=()=>{
-      this.setState({
-          next: this.state.next + 1
-      })
-  }
-  displayInput=()=>{
-    if(this.state.next === 0){
-        return 'display-1'
-    }else if (this.state.next=== 1 && this.state.name !== ""){
-        return '-2'
-    }else if(this.state.next===2 && this.state.name !== '' && this.state.email !==3){
-        
-        return 'play-3'
-    }else if(this.state.next > 2){
-        this.setState({
-            next: 2
-        })
+  nextCounter = () => {
+    this.setState({
+      next: this.state.next + 1,
+      writtenName: this.state.name !== "" ? this.state.name : null,
+      writtenEmail: this.state.email !== "" ? this.state.email : null,
+      writtenMessage:
+        this.state.email !== "" && this.state.next >= 2
+          ? "Now write your message"
+          : null,
+    });
+  };
+  displayPreWords = () => {
+    return (
+      <div className="pre-words">
+        {this.state.writtenName !== "" && this.state.next >= 1 ? (
+          <span className="pw-wording">
+            <i className="fas fa-user-circle fa-2x"></i>{" "}
+            {this.state.writtenName}
+          </span>
+        ) : null}
+        {this.state.writtenEmail !== "" && this.state.next >= 2? (
+          <span className="pw-wording">
+            <i className="fas fa-envelope fa-2x"></i> {this.state.writtenEmail}
+          </span>
+        ) : null}
+        {this.state.message === String && this.state.next >= 2 ? (
+          <span className="pw-wording">
+            <i className="fas fa-pen-square fa-2x"></i>{" "}
+            {this.state.writtenMessage}
+          </span>
+        ) : null}
+      </div>
+    );
+  };
+  displayInput = () => {
+    if (this.state.next === 0) {
+      return "display-1";
+    } else if (this.state.next === 1 && this.state.name !== "") {
+      return "-2";
+    } else if (
+      this.state.next === 2 &&
+      this.state.name !== "" &&
+      this.state.email !== 3
+    ) {
+      return "play-3";
+    } else if (this.state.next > 2) {
+      
+      return "play-3";
     }
-  }
+  };
   render() {
     const { status } = this.state;
     return (
@@ -44,39 +75,57 @@ export default class Form extends React.Component {
         action="https://formspree.io/moqkwdep"
         method="POST"
         className="form-box"
-      > 
-        <input id={this.displayInput()} 
-          onChange={this.handleChange}
-          className="form-in"
-          type="text"
-          name="name"
-          placeholder="Name"
-        />
-
-        <input id={`display${this.displayInput()}`}
-          onChange={this.handleChange}
-          className="form-in"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-
-        <input id={`dis${this.displayInput()}`}
-          onChange={this.handleChange}
-          className="form-in"
-          type="text"
-          name="message"
-          placeholder="Message"
-        />
+      >
+      
+      {this.displayPreWords()}
+        <div id={this.displayInput()} className="input-box">
+          <input
+            onChange={this.handleChange}
+            className="form-in"
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+          <span id="input-icon">
+            <i className="fas fa-user-circle fa-2x"></i>
+          </span>
+        </div>
+        <div id={`display${this.displayInput()}`} className="input-box">
+          <input
+            onChange={this.handleChange}
+            className="form-in"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <span id="input-icon">
+            <i className="fas fa-envelope fa-2x"></i>
+          </span>
+        </div>
+        <div id={`dis${this.displayInput()}`} className="input-box">
+          <input
+            onChange={this.handleChange}
+            className="form-in"
+            type="text"
+            name="message"
+            placeholder="Message"
+            onInput={this.displayPreWords}
+          />
+          <span id="input-icon">
+            <i className="fas fa-pen-square fa-2x"></i>
+          </span>
+        </div>
         {console.log(this.state)}
-        {console.log(this.state.next)}
+        {/* {console.log(this.state.next)} */}
         {status === "SUCCESS" ? (
           <p>Thanks!</p>
         ) : (
           <button className="button">Submit</button>
         )}
         {status === "ERROR" && <p>Ooops! There was an error.</p>}
-        <button type='button' onClick={this.nextCounter} >Next</button>
+        <button type="button" onClick={this.nextCounter}>
+          Next
+        </button>
       </form>
     );
   }
