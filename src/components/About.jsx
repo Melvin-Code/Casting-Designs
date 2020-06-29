9075cd088e329525af4cbc907e06d340764f8762
melvin-Code <melvingonzalez470@gmail.com>
2020-06-29 17:49:35 -0400

    redoing the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,41 +4,29 @@ import Skills from "./partials/Skills";
 import Background from "./partials/Background";
 
 class About extends Component {
-  state = {
-    indicator: true,
-    counter: 0,
-    bkDis: 'block',
-    bkName: 'fromLeft',
-    skDis: 'none',
-    skName: 'FromCt',
-  };
+//   state = {
+//     indicator: true,
+//     counter: 0,
+//     bkDis: 'block',
+//     bkName: 'fromLeft',
+//     skDis: 'none',
+//     skName: 'FromCt',
+//   };
 
   
-  setter=(first, second, third, forth)=>{
+//   setter=(first, second, third, forth)=>{
   
-    this.setState({
-      [first]: 'FromCt',
-      [second]: 'none',
-      [third]: 'fromLeft',
-      [forth]: 'block'
-    })
+//     this.setState({
+//       [first]: 'FromCt',
+//       [second]: 'none',
+//       [third]: 'fromLeft',
+//       [forth]: 'block'
+//     })
   
-}
+// }
   render() {
     return (
       <div className="about-page setter">
-        <Background
-          display={this.state.bkDis}
-          animationDuration="2s"
-          animationName={this.state.bkName}
-          setter = {this.setter}
-        />
-        <Skills
-          display={this.state.skDis}
-          animationDuration="2s"
-          animationName={this.state.skName}
-          setter={this.setter}
-        />
         {console.log(this.state)}
       </div>
     );
@@ -46,3 +34,16 @@ class About extends Component {
 }
 
 export default About;
+
+// {/* <Background
+//   display={this.state.bkDis}
+//   animationDuration="2s"
+//   animationName={this.state.bkName}
+//   setter = {this.setter}
+// />
+// <Skills
+//   display={this.state.skDis}
+//   animationDuration="2s"
+//   animationName={this.state.skName}
+//   setter={this.setter}
+// /> */}
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class Work extends Component {
     render() {
+
         return (
-            <div>
-                work
+            <div class='container'>
+                <div>
+                    jsdkghsdfhksfjhlbsdjfhbdjhvbsnv jdbcjkbvw
+                </div>
+                <div>
+                    khfbjldhfhsdkjfbhdkjhfbjsdkfgvdshjbdhskfgjshdfgsdfvsh
+                </div>
             </div>
         );
     }
