7a13d76f0e9c4e91a4e7b02b6ea1fb695f09b3c8
melvin-Code <melvingonzalez470@gmail.com>
2020-07-03 13:57:26 -0400

    contact page animation added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Work extends Component {
-    render() {
-
-        return (
-            <div class='container'>
-                <div>
-                    jsdkghsdfhksfjhlbsdjfhbdjhvbsnv jdbcjkbvw
-                </div>
-                <div>
-                    khfbjldhfhsdkjfbhdkjhfbjsdkfgvdshjbdhskfgjshdfgsdfvsh
-                </div>
+  render() {
+    return (
+      <div class="container">
+        <div>
+          <div>
+            <img></img>
+            <h3></h3>
+            <img></img>
+          </div>
+          <div>
+            <p></p>
+          </div>
+          <div>
+            <div>
+              <svg></svg>
+              <p></p>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Work;
